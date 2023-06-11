0ab47951bc3382c4d821d4b519df8f243bc87828
Andriy Kalinichenko <39601026+Mo-ja-ve@users.noreply.github.com>
2023-06-11 13:13:23 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -8,28 +8,52 @@
 
 //pin controls for GM Series 349 and 349H
 double pins[15] = {
-        0.06,  // 1 
-        0.13,  // 2
-         0.0,  // 3  ANALOG INPUT / STROBE LATCH
-         0.0,  // 4  GND
-        0.25,  // 5
-        0.5,   // 6
-        1.0,   // 7
-        2.0,   // 8
-        4.0,   // 9
-        8.0,   // 10
-        16.0,  // 11
-        32.0,  // 12
-        0.0,   // 13  +12V TO +15V
-        0.0,   // 14  -12V TO -15V
-        0.03   // 15
-    };
+    0.06,   // 1 
+    0.13,   // 2
+    0.0,    // 3  ANALOG INPUT / STROBE LATCH
+    0.0,    // 4  GND
+    0.25,   // 5
+    0.5,    // 6
+    1.0,    // 7  //57
+    2.0,    // 8
+    4.0,    // 9
+    8.0,    // 10 //57
+    16.0,   // 11 //57
+    32.0,   // 12 //57
+    0.0,    // 13  +12V TO +15V
+    0.0,    // 14  -12V TO -15V
+    0.03    // 15
+};
+
+//  last 2 bits are used to set number of attenuators used 1 through 4  ( [0,1,2,3] )
+//  bit set 1 indicates that pin will be set on, bit set 0 indicates pin that pin will be set off
+char lookUp[15] = {
+    0b10000000, // 0.06
+    0b01000000, // 0.13
+    0b00100000, // 0.0   // ANALOG INPUT / STROBE LATCH
+    0b00010000, // 0.0   // GND
+    0b00001000, // 0.25
+    0b00000100, // 0.5
+    0b00000010, // 1.0
+    0b00000001, // 2.0
+    0b10000000, // 4.0
+    0b01000000, // 8.0
+    0b00100000, // 16.0
+    0b00010000, // 32.0 
+    0b00001000, // 0.0   // +12V TO +15V
+    0b00000100, // 0.0   // -12V TO -15V
+    0b00000100  // 0.03
+};
+
+//  00000010 01110000
 
 HANDLE hDevice;  // Handle to the USB device
 char *writeData;  // Data to be written
 DWORD bytesWritten, bytesRead;  // Variable to store the number of bytes written
 bool run = 1;
 DCB dcbSerialParams;
+
+void divideUp(char writeData[]);
 
 int main(){
 
@@ -81,62 +105,41 @@ int main(){
 	printf("Enter: ");
 	double temp =0.0;
 	scanf("%lf", &temp);
-	if(temp >= 10.0){
-		writeData[1] = (char)((int)floor(temp));
-		temp = temp - floor(temp);
-		temp *= 100;
-		writeData[2] = (char)temp;
-	}else{
-		writeData[1] = (char)((int)temp);
-	}
-	char transfer1, transfer2 = (char)0;
-	if(writeData[2] == 0){
-		lvldB = (int)writeData[1];
-		for(int i = 14; i >= 0 ; i--){
-			if(pins[i] != 0.0 && (int)pins[i] != 0.0){
-				if(lvldB % (int)pins[i] == 0){
-					switch(i+1){
-						case 7:
-							transfer1 = transfer1 | 0b00000010;
-							break;
-						case 8:
-							transfer1 = transfer1 | 0b00000001;
-							break;
-						case 9:
-							transfer2 = transfer2 | 0b10000000;
-							break;
-						case 10:
-							transfer2 = transfer2 | 0b0100000;
-							break;
-						case 11:
-							transfer2 = transfer2 | 0b00100000;
-							break;
-						case 12:
-							transfer2 = transfer2 | 0b00010000;
-							break;
-					break;
-					}
-					lvldB -= (int)pins[i];
-				}
-				printf("MOD: %d\n",lvldB);
-			}
-		}
-	}
+	writeData[1] = (char)((int)floor(temp));
+	temp = temp - floor(temp);
+	//printf("HELLO: %lf", temp);
+	temp *= 100;
+	writeData[2] = (char)temp;
+
+	// printf("\n");
+	// printf("write data: %d", writeData[0]);
+	// printf("\n");
+	// printf("write data: %d", writeData[1]);
+	// printf("\n");
+	// printf("write data: %d", writeData[2]);
+	// printf("\n");
 	
 	do {
-		if(!WriteFile(hDevice, writeData, strlen(writeData), &bytesWritten, NULL)) {
+		divideUp(writeData);
+		printf("WRITE DATA: %d\n", writeData[0]);
+		printf("WRITE DATA: %d\n", writeData[1]);
+
+		if(!WriteFile(hDevice, writeData, 2, &bytesWritten, NULL)) {
 			printf("Failed to write to the USB device. Error code: %lu\n", GetLastError());
 			CloseHandle(hDevice);
 			return 1;
 		}else{
-			printf("Wrote %d bytes to controller succesfully, enter r to stop and reset or q to quit\n",bytesWritten);
+			printf("Wrote 2 bytes to controller succesfully, enter r to stop and reset or q to quit\n",bytesWritten);
 		}
 		while (1){
 			printf("Waiting for return ...\n");
         	if (ReadFile(hDevice, buffer, sizeof(buffer) - 1, &bytesRead, NULL)){
             	if (bytesRead > 0){
                 	buffer[bytesRead] = '\0';
-                	printf("Received data: %s\n", buffer);
+
+					for(int i = 0; i < bytesRead; i++)
+                		printf("Received data: %d\n", buffer[i]);
+
 					break;
             	}
         	}
@@ -147,7 +150,117 @@ int main(){
 		}
 		if(tempChar[0] == 'r');
 	}while(run);
-
 	//printf("Data written to the USB device: %s\n", writeData);
+}
 
+
+void divideUp(char writeData[]){
+
+		char transfer1, transfer2 = (char)0;
+		int lvldB2 = (int)writeData[2];
+		int lvldB1 = (int)writeData[1];
+		double temp = 0.0;
+
+		for(int i = 14; i >= 0 ; i--){
+			
+			if(lvldB2 == 0)
+				break;
+			temp = pins[i]*100.0;
+
+			if( lvldB2 == (int)(temp) ){
+				//printf("hello!");
+				if(i <= 7){
+					writeData[0] = lookUp[i]  | transfer1;
+					writeData[1] = transfer2;
+					break;
+				}else{
+					writeData[0] = transfer1;
+					writeData[1] = lookUp[i]  | transfer2;
+					printf("hello!: %d\n", writeData[1]);
+					break;
+				}
+			}
+
+			if(pins[i] < 1.0 && pins[i] != 0){
+				if(lvldB2 % (int)(pins[i]*100) != 0 && lvldB2 % (int)(pins[i]*100) != lvldB2){
+					printf("hello!");
+					switch(i+1){
+						case 1:
+							transfer1 = transfer1 | 0b10000000;
+							lvldB2 -= (int)pins[i]*100;
+							break;
+						case 2:
+							transfer1 = transfer1 | 0b01000000;
+							lvldB2 -= (int)pins[i]*100;
+							break;
+						case 5:
+							transfer1 = transfer1 | 0b00001000;
+							lvldB2 -= (int)pins[i]*100;
+							break;
+						case 6:
+							transfer1 = transfer1 | 0b00000100;
+							lvldB2 -= (int)pins[i]*100;
+							break;
+						case 15:
+							transfer2 = transfer2 | 0b00000100;
+							lvldB2 -= (int)pins[i]*100;
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		}
+
+		for(int i = 14; i >= 0 ; i--){
+			
+			if(lvldB1 == 0)
+				return;
+
+			if(lvldB1 == (int)pins[i]){
+				if(i <= 7){
+					writeData[0] = lookUp[i]  | transfer1;
+					//writeData[1] = transfer2;
+					return;
+				}else{
+					//writeData[0] = transfer1;
+					writeData[1] = lookUp[i]  | transfer2;
+					return;
+				}
+			}
+
+			if(pins[i] >= 1.0){
+				if(lvldB1 % (int)pins[i] != 0 && lvldB1 % (int)pins[i] != lvldB1){
+					switch(i+1){
+						case 7:
+							transfer1 = transfer1 | 0b00000010;
+							lvldB1 -= (int)pins[i];
+							break;
+						case 8:
+							transfer1 = transfer1 | 0b00000001;
+							lvldB1 -= (int)pins[i];
+							break;
+						case 9:
+							transfer2 = transfer2 | 0b10000000;
+							lvldB1 -= (int)pins[i];
+							break;
+						case 10:
+							transfer2 = transfer2 | 0b01000000;
+							lvldB1-= (int)pins[i];
+							break;
+						case 11:
+							transfer2 = transfer2 | 0b00100000;
+							lvldB1 -= (int)pins[i];
+							break;
+						case 12:
+							transfer2 = transfer2 | 0b00010000;
+							lvldB1 -= (int)pins[i];
+							break;
+					default:
+						break;
+					}
+				}
+				//printf("MOD: %d\n",lvldB);
+			}
+		}
 }
